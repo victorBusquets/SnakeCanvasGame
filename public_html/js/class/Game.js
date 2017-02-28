e65e0d60567853214b127c065f8cd8ea490fa78c
Victor Busquets <@noteimporta34>
2017-02-28 11:44:12 +0100

    Snake movement animation

--- public_html/js/class/Game.js
+++ public_html/js/class/Game.js
@@ -1,6 +1,18 @@
 function Game ( gameSize ){
 	var canvas 	= new Canvas( "game", gameSize, true ),
-		snake 	= new Snake();
-				
-	snake.paint( canvas );
+		snake 	= new Snake( gameSize ),
+		speed 	= 300; 	
+
+	function init(){		
+		snake.paint( canvas );
+		loop();
+	};
+	
+	function loop(){
+		setInterval(function(){
+			snake.loop( canvas );
+		}, speed);
+	};
+	
+	init();
 };
--- public_html/js/class/Snake.js
+++ public_html/js/class/Snake.js
@@ -1,5 +1,6 @@
-function Snake(){
-	var nodes = new Array();
+function Snake( gameSize ){
+	var nodes = new Array(),
+		direction = 0;
 	
 	function prepareSnake(){
 		nodes[0] = new Node( new Position( 0, 0 ), 0, assets.snakeTop );
@@ -25,6 +26,73 @@ function Snake(){
 		}
 	};
 	
+	function getUpdateDirectionValues(){
+		var values;
+		
+		switch(direction) {
+			case 0:
+				values = { x: 0, y: -1 };
+				break;
+			case 1:
+				values = { x: 1, y: 0 };
+				break;
+			case 2:
+				values = { x: 0, y: 1 };
+				break;
+			default:
+				values = { x: -1, y: 0 };
+		}
+		
+		return values;
+	};
+	
+	function newLimitPosition(position){
+		var x = position.getX(),
+			y = position.getY();
+			
+		position.setX( x<0 ? gameSize.x-1 : ( x>gameSize.x ? 0 : x ) );
+		position.setY( y<0 ? gameSize.y-1 : ( y>gameSize.y ? 0 : y ) );
+		
+		return position;
+	};
+	
+	function outOfCanvas(position){
+		return position.getX()<0 || position.getY()<0 || position.getX()>gameSize.x-1 || position.getX()>gameSize.y-1;
+	};
+	
+	function createNewNode(){
+		var nodeToClone  = nodes[0],
+			updateValues = getUpdateDirectionValues();
+			position 	 = new Position( nodeToClone.getPosition().getX() + updateValues.x, nodeToClone.getPosition().getY() + updateValues.y );
+		
+		position = ( outOfCanvas( position ) ? newLimitPosition(position) : position );
+		
+		return new Node( position, direction, assets.snakeTop );
+	};
+	
+	function updateStartNode(){
+		nodes[0].setImg( assets.snakeMid );
+	};
+	
+	function updateEndNode(){
+		nodes[nodes.length-1].setImg( assets.snakeBot );
+	};
+	
+	function update(){
+		var node = createNewNode();
+	
+		nodes.pop();	//REMOVING LAST NODE	
+		updateStartNode();
+		updateEndNode();
+		nodes.unshift( node ); //ADDING NEW NODE
+	};
+	
+	function loop(canvas){
+		clear( canvas );
+		update();
+		paint( canvas );
+	};
+	
 	function init(){
 		prepareSnake();
 	};
@@ -33,6 +101,7 @@ function Snake(){
 
 	return {
 		paint: paint,
+		loop: loop,
 		clear: clear
 	}
 };
